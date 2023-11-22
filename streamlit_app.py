488d69c24fab9550444e1f71aa9906aef0553c05
Paolo Timoni <51727538+paolotim@users.noreply.github.com>
2023-11-22 15:36:25 -0500

    Added title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 # created the main python file
-# Print Welcome Message
-message = 'Hello World'
-message2 = "Paolo's World"
-message3 = """Paolo's world is a fantastic place
-that includes several unique experiences."""
+import streamlit
+
+streamlit.title('My Parents New Healthy Diner')
