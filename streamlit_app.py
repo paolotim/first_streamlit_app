da40f079da164ac89735d127e346f71a38036894
Paolo Timoni <51727538+paolotim@users.noreply.github.com>
2023-11-22 15:34:08 -0500

    Added test messages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
 # created the main python file
+# Print Welcome Message
+message = 'Hello World'
+message2 = "Paolo's World"
+message3 = """Paolo's world is a fantastic place
+that includes several unique experiences."""
