9577ae8cd2eb6a5d1a099375efe930e4f08e4e77
Paolo Timoni <51727538+paolotim@users.noreply.github.com>
2023-11-23 16:15:14 -0500

    new edit for fruit choice picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,10 @@ st.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # st.text(fruityvice_response.json()) # just writes the data to the screen
 
 # normalize the json response 
