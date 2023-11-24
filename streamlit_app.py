fbd5d302b6ed981fe2197267d9a05512d6e2b881
Paolo Timoni <51727538+paolotim@users.noreply.github.com>
2023-11-24 13:02:53 -0500

    better version for fruitvice picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,30 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
+# First simple version
+# st.header("Fruityvice Fruit Advice!")
+# fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+# st.write('The user entered ', fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# normalize the json response 
+# fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# display the normalize table on the screen
+# st.dataframe(fruityvice_normalized)
+# Better production version
 st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+except URLError as 2:
+  st.error()
+# end of new version
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 # st.text(fruityvice_response.json()) # just writes the data to the screen
 
 # normalize the json response 
