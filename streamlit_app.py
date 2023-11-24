3cf5d449170e7eb1569246ec4e6af752d55aa8fa
Paolo Timoni <51727538+paolotim@users.noreply.github.com>
2023-11-24 13:18:51 -0500

    fixing bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ st.dataframe(fruits_to_show)
 
 # Better production version using functions
 # function definition
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
