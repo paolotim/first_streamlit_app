d363ec42ce1d37a325cda25daed073679b336e47
Paolo Timoni <51727538+paolotim@users.noreply.github.com>
2023-11-24 13:18:08 -0500

    frutyvice api with function

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,17 +34,24 @@ st.dataframe(fruits_to_show)
 # fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # display the normalize table on the screen
 # st.dataframe(fruityvice_normalized)
-# Better production version
+
+# Better production version using functions
+# function definition
+def get_fruityvice_data(this_fruit_choice)
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+# display fruityvice api response
 st.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = st.text_input('What fruit would you like information about?')
   if not fruit_choice:
     st.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    st.dataframe(fruityvice_normalized)
-    
+    back_from_function = get_fruityvice_data(fruit_choice)
+    st.dataframe(back_from_function)
+
 except URLError as e:
   st.error()
 # end of new version
